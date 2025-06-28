c24d2b0334380b2856cf91e77571ac7cc9143dc4
wsf <alejandro.sartorio@gmail.com>
2025-06-27 21:16:48 -0300

    Update product.py

--- models/product.py
+++ models/product.py
@@ -3940,7 +3940,7 @@ class product_product(models.Model):
             if ("mrp.bom" in self.env):
             	product_id = var
                #check all boms of this kit
-		bom_ids = self.env['mrp.bom'].search([('product_id','=',product_id.id)]) or []
+		        bom_ids = self.env['mrp.bom'].search([('product_id','=',product_id.id)]) or []
                 _st_mv_ids = _st_mv_ids or self.env['stock.move']
                 for bom_id in bom_ids:
                     if (not bom_id or not bom_id.bom_line_ids):
